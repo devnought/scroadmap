e66855772f09e417dc800c63d40de1609adbeaed
Kyle Gretchev <kgretchev@gmail.com>
2018-07-15 11:46:29 -0400

    Trying other formats other than JSON

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
     - [ ] Client will maybe do the diffing, need to see compressed payload sizes
 - [ ] Compress payloads - [Snappy](https://github.com/BurntSushi/rust-snappy)
     - [ ] JS will get the data, WASM will handle decompression
+- [ ] Enforce access via tokens?
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+extern crate bincode;
 extern crate reqwest;
 extern crate serde;
 #[macro_use]
@@ -17,6 +18,13 @@ pub fn get_stuff() {
 
     let payload = res.json::<Payload>().expect("Could not get JSON result");
 
+    write_json(&payload);
+    write_json_compressed(&payload);
+    write_bindocde(&payload);
+    write_bindocde_compressed(&payload);
+}
+
+fn write_json(payload: &Payload) {
     let file_name = format!(
         "payloads/{}.json",
         payload.data().expect("No payload data").last_updated()
@@ -26,4 +34,42 @@ pub fn get_stuff() {
     let writer = BufWriter::new(file);
 
     serde_json::to_writer(writer, &payload).expect("Could not write data to file");
+}
+
+fn write_json_compressed(payload: &Payload) {
+    let file_name = format!(
+        "payloads/{}.json.cmp",
+        payload.data().expect("No payload data").last_updated()
+    );
+
+    let file = File::create(file_name).expect("Could not create file");
+    let writer = BufWriter::new(file);
+    let snap_write = snap::Writer::new(writer);
+
+    serde_json::to_writer(snap_write, &payload).expect("Could not write data to file");
+}
+
+fn write_bindocde(payload: &Payload) {
+    let file_name = format!(
+        "payloads/{}.bin",
+        payload.data().expect("No payload data").last_updated()
+    );
+
+    let file = File::create(file_name).expect("Could not create file");
+    let writer = BufWriter::new(file);
+
+    bincode::serialize_into(writer, &payload).expect("Could not write data to file");
+}
+
+fn write_bindocde_compressed(payload: &Payload) {
+    let file_name = format!(
+        "payloads/{}.bin.cmp",
+        payload.data().expect("No payload data").last_updated()
+    );
+
+    let file = File::create(file_name).expect("Could not create file");
+    let writer = BufWriter::new(file);
+    let snap_write = snap::Writer::new(writer);
+
+    bincode::serialize_into(snap_write, &payload).expect("Could not write data to file");
 }
