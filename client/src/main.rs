1e3d0fa32b3ac7fe03a3a07db6f4c42224155b48
Kyle Gretchev <kgretchev@gmail.com>
2018-07-23 22:51:28 -0400

    Added dummy client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,45 @@
 #![feature(rust_2018_preview)]
 #![warn(rust_2018_idioms)]
 
+use yew::{prelude::*, html, html_impl};
+
+struct Model {}
+
+enum Msg {
+    DoIt,
+}
+
+impl Component for Model {
+    // Some details omitted. Explore the examples to see more.
+
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Model {}
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::DoIt => {
+                // Update your model on events
+                true
+            }
+        }
+    }
+}
+
+impl Renderable<Model> for Model {
+    fn view(&self) -> Html<Self> {
+        html! {
+            // Render your model here
+            <button onclick=|_| Msg::DoIt,>{ "Click me!" }</button>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::initialize();
+    App::<Model>::new().mount_to_body();
+    yew::run_loop();
 }
