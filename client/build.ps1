917e778303e97054b71c69a2a9a542437f74f088
Kyle Gretchev <kgretchev@gmail.com>
2018-07-29 19:13:20 -0400

    No more webpack

--- client/build.ps1
+++ client/build.ps1
@@ -1,2 +1,2 @@
 cargo +nightly build --release --target wasm32-unknown-unknown
-wasm-bindgen ./target/wasm32-unknown-unknown/release/client.wasm --out-dir ./static
+wasm-bindgen ./target/wasm32-unknown-unknown/release/client.wasm --no-modules --browser --out-dir ./static
--- client/static/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const js = import("./client");
-
-js.then(js => {
-    js.greet("World!");
-});
