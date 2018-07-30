da9b19dbe3b3320ff2a3cd702acb7c16db7f99c8
Kyle Gretchev <kgretchev@gmail.com>
2018-07-29 21:54:40 -0400

    Updates

--- client/build.ps1
+++ client/build.ps1
@@ -1,2 +1,3 @@
 cargo +nightly build --release --target wasm32-unknown-unknown
 wasm-bindgen ./target/wasm32-unknown-unknown/release/client.wasm --no-modules --browser --out-dir ./static
+squash ./static/client.js -o ./static/client_ugly.js
